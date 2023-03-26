06e0955eebe3bc253c781f5f5d0afc15f2031361
Devashish Rathore <devrathod96445@gmail.com>
2023-03-27 00:38:12 +0530

    part2 done

--- lambdas/getUsers.js
+++ lambdas/getUsers.js
@@ -1,10 +1,10 @@
 "use strict";
 const fs = require("fs");
 const User = require("./models/user");
-const DB = require("./db");
 const Responses = require("./Api_Responses");
 
-exports.handler = async (event) => {
+// From JSON File
+exports.getAllUsers = async (event) => {
   try {
     console.log("event", event);
     const { limit, offset } = event.queryStringParameters || {};
@@ -19,7 +19,7 @@ exports.handler = async (event) => {
       event.queryStringParameters !== limit ||
       event.queryStringParameters !== offset
     ) {
-      Responses._400({ message: "invalid Parameter" });
+      Responses._400({ message: "invalid Parameter" }); // confusion in this Block code (how to check unknown parameter)
     }
     // Validate the limit and offset values
     if (parsedLimit !== null && (isNaN(parsedLimit) || parsedLimit <= 0)) {
@@ -53,8 +53,51 @@ exports.handler = async (event) => {
   }
 };
 
-const users = await DB.collection("User")
-  .find()
-  .skip(Number(parsedOffset))
-  .limit(Number(parsedLimit));
-toArray();
+// From DB
+exports.getAllUsers = async (event) => {
+  try {
+    const { limit, offset } = event.queryStringParameters || {};
+    const parsedLimit = limit ? parseInt(limit, 10) : null;
+    const parsedOffset = offset ? parseInt(offset, 10) : null;
+    // validate unknown parameter
+    if (
+      event.queryStringParameters !== null ||
+      event.queryStringParameters !== limit ||
+      event.queryStringParameters !== offset
+    ) {
+      Responses._400({ message: "invalid Parameter" }); // confusion in this Block code (how to check unknown parameter)
+    }
+    // Validate the limit and offset values
+    if (parsedLimit !== null && (isNaN(parsedLimit) || parsedLimit <= 0)) {
+      throw new Error("Invalid limit parameter");
+    }
+    if (parsedOffset !== null && (isNaN(parsedOffset) || parsedOffset < 0)) {
+      throw new Error("Invalid offset parameter");
+    }
+    if (
+      !event.queryStringParameters ||
+      !event.queryStringParameters.limit ||
+      !event.queryStringParameters.offset
+    ) {
+      // without pagination
+      const users = await User.find().toArray();
+      if (!users && users.length == 0) {
+        // no users found error
+        return Responses._404({ message: "No users Found" });
+      }
+      return Responses._200(users);
+    } else {
+      const allUsers = await User.find()
+        .skip(Number(parsedOffset))
+        .limit(Number(parsedLimit))
+        .toArray();
+      if (!allUsers && allUsers.length == 0) {
+        // no users found error
+        return Responses._404({ message: "No users Found" });
+      } // return users
+      return Responses._200(allUsers);
+    }
+  } catch (error) {
+    return Responses._500({ message: error.message });
+  }
+};
